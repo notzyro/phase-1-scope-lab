d77a7084bbfd6cda4407394e61d34c6918220972
Edward Angelo Salas <zyro670@gmail.com>
2022-03-24 23:49:28 -0400

    commit tree 4d3d74b74f41fb1e59af8d3addea89a68a74f67d

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+return customerName;
+}
+function setBestCustomer() {
+    return bestCusomter;
+}
+var bestCusomter = 'not bob';
